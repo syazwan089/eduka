8ac96002d8616b433666250ce3d3a62d4ac3a152
mohd nur syazwan <syazwan089@gmail.com>
2020-01-20 11:44:04 +0800

    logout

---
+++ Eduka/Views/Logout.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Eduka.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Logout : ContentPage
+    {
+        public Logout()
+        {
+            InitializeComponent();
+            LogoutFunc();
+        }
+
+        private async void LogoutFunc()
+        {
+            bool x = await DisplayAlert("Adakah anda pasti?", "Anda boleh log masuk pada bila-bila masa", "Logout", "Cancel");
+            if(x)
+            {
+                Preferences.Remove("userid");
+                App.Current.MainPage = new NavigationPage(new StartPage());
+            }
+
+            else
+            {
+                App.Current.MainPage = new AppShell();
+            }
+        }
+    }
+}
